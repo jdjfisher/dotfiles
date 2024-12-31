dd940fda58218ccabbcf7a7f147552412ce811f6
jdjfisher <43887886+jdjfisher@users.noreply.github.com>
2024-12-31 11:54:35 +0000

    Patch install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,9 @@
+#!/bin/bash
 
 # TODO: Prompt file replacement
 
 # Resolve dotfiles repository absolute path 
-DOTFILES_PATH=$(dirname $(realpath -s $0))
+DOTFILES_PATH=$(dirname $(realpath $0))
 
 # Create profile symlink in the home directory 
 ln -sfv "$DOTFILES_PATH/.bashrc" "$HOME/.bashrc"
